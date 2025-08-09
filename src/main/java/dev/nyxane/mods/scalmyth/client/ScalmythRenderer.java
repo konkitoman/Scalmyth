b02693549f8dd8a9fefa18d08dace4161aa0fe63
Konkitoman <konkitoman@users.noreply.github.com>
2025-08-09 21:34:49 +0300

    Add emisive_textures and base ai the ai is made by @Aubarino
    ---------
    Co-Authored-By: Aubarino <53990174+Aubarino@users.noreply.github.com>

--- src/main/java/dev/nyxane/mods/scalmyth/client/ScalmythRenderer.java
+++ src/main/java/dev/nyxane/mods/scalmyth/client/ScalmythRenderer.java
@@ -3,10 +3,13 @@ package dev.nyxane.mods.scalmyth.client;
 import dev.nyxane.mods.scalmyth.entity.scalmyth.ScalmythEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class ScalmythRenderer extends GeoEntityRenderer<ScalmythEntity> {
 
     public ScalmythRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new ScalmythModel());
+
+        addRenderLayer(new AutoGlowingGeoLayer<>(this));
     }
 }
