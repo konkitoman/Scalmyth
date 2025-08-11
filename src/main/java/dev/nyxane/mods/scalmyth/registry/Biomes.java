fe820156e0ded41eb6477bf5ce9e2ab2527d0004
Konkitoman <konkitoman@users.noreply.github.com>
2025-08-11 15:09:03 +0300

    No more java reflection we are now using Access Transformers

--- src/main/java/dev/nyxane/mods/scalmyth/registry/Biomes.java
+++ src/main/java/dev/nyxane/mods/scalmyth/registry/Biomes.java
@@ -1,7 +1,6 @@
 package dev.nyxane.mods.scalmyth.registry;
 
 import net.minecraft.world.level.biome.*;
-import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 import net.neoforged.neoforge.event.server.ServerAboutToStartEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -42,38 +41,22 @@ public class Biomes {
             if (dimensionType.is(BuiltinDimensionTypes.OVERWORLD)) {
                 ChunkGenerator chunkGenerator = levelStem.generator();
                 // Inject biomes to biome source
-                try {
                 if (chunkGenerator.getBiomeSource() instanceof MultiNoiseBiomeSource noiseSource) {
-                  java.lang.reflect.Method noiseSource$parameters = MultiNoiseBiomeSource.class.getDeclaredMethod("parameters");
-                  java.lang.reflect.Field chunkGenerator$biomeSource = ChunkGenerator.class.getDeclaredField("biomeSource");
-                  java.lang.reflect.Field chunkGenerator$featuresPerStep = ChunkGenerator.class.getDeclaredField("featuresPerStep");
-                  java.lang.reflect.Field chunkGenerator$generationSettingsGetter = ChunkGenerator.class.getDeclaredField("generationSettingsGetter");
-                  chunkGenerator$biomeSource.setAccessible(true);
-                  noiseSource$parameters.setAccessible(true);
-                  chunkGenerator$featuresPerStep.setAccessible(true);
-                  chunkGenerator$generationSettingsGetter.setAccessible(true);
-
-                  List<Pair<Climate.ParameterPoint, Holder<Biome>>> parameters = new ArrayList<>(((Climate.ParameterList<Holder<Biome>>)noiseSource$parameters.invoke((Object)noiseSource)).values());
+                  List<Pair<Climate.ParameterPoint, Holder<Biome>>> parameters = new ArrayList<>(noiseSource.parameters().values());
 
                     addParameterPoint(parameters, new Pair<>(new Climate.ParameterPoint(Climate.Parameter.span(-0.3f, 1.5f), Climate.Parameter.span(-1.5f, 0.1f), Climate.Parameter.span(-0.5f, 1.2f), Climate.Parameter.span(-2f, -0.2f),
                             Climate.Parameter.point(0.0f), Climate.Parameter.span(-0.3f, 1f), 0), biomeRegistry.getHolderOrThrow(ResourceKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("scalmyth", "ashen_biome")))));
                     addParameterPoint(parameters, new Pair<>(new Climate.ParameterPoint(Climate.Parameter.span(-0.3f, 1.5f), Climate.Parameter.span(-1.5f, 0.1f), Climate.Parameter.span(-0.5f, 1.2f), Climate.Parameter.span(-2f, -0.2f),
                             Climate.Parameter.point(1.0f), Climate.Parameter.span(-0.3f, 1f), 0), biomeRegistry.getHolderOrThrow(ResourceKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("scalmyth", "ashen_biome")))));
                     MultiNoiseBiomeSource biomeSource = MultiNoiseBiomeSource.createFromList(new Climate.ParameterList<>(parameters));
-                    chunkGenerator$biomeSource.set(chunkGenerator, biomeSource);
-                    Function<Holder<Biome>, BiomeGenerationSettings> generationSettings = (Function<Holder<Biome>, BiomeGenerationSettings>)chunkGenerator$generationSettingsGetter.get(chunkGenerator);
-                    chunkGenerator$featuresPerStep.set(chunkGenerator, Suppliers
-                            .memoize(() -> FeatureSorter.buildFeaturesPerStep(List.copyOf(biomeSource.possibleBiomes()), biome -> generationSettings.apply(biome).features(), true)));
+                    chunkGenerator.biomeSource = biomeSource;
+                    Function<Holder<Biome>, BiomeGenerationSettings> generationSettings = chunkGenerator.generationSettingsGetter;
+                    chunkGenerator.featuresPerStep = Suppliers
+                            .memoize(() -> FeatureSorter.buildFeaturesPerStep(List.copyOf(biomeSource.possibleBiomes()), biome -> generationSettings.apply(biome).features(), true));
 
                 }
                 if (chunkGenerator instanceof NoiseBasedChunkGenerator noiseGenerator) {
-                    java.lang.reflect.Field noiseGenerator$settings = NoiseBasedChunkGenerator.class.getDeclaredField("settings");
-                    noiseGenerator$settings.setAccessible(true);
-
-                    ((ScalmythModNoiseGeneratorSettings) (Object) (((Holder<NoiseGeneratorSettings> )noiseGenerator$settings.get(noiseGenerator)).value())).setscalmythDimensionTypeReference(dimensionType);
-                }
-                } catch (ReflectiveOperationException e) {
-                  throw new RuntimeException(e);
+                  ((ScalmythModNoiseGeneratorSettings) (Object) noiseGenerator.settings.value()).setscalmythDimensionTypeReference(dimensionType);
                 }
             }
         }
@@ -89,21 +72,14 @@ public class Biomes {
         List<SurfaceRules.RuleSource> customSurfaceRules = new ArrayList<>();
         customSurfaceRules.add(preliminarySurfaceRule(ResourceKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("scalmyth", "ashen_biome")), dev.nyxane.mods.scalmyth.registry.Blocks.ASHEN_GRASS.get().defaultBlockState(), Blocks.DIRT.defaultBlockState(),
                 Blocks.GRAVEL.defaultBlockState()));
-        try{
-          Class<?> SurfaceRules$SequenceRuleSource = Class.forName("net.minecraft.world.level.levelgen.SurfaceRules$SequenceRuleSource");
-          java.lang.reflect.Field SurfaceRules$SequenceRuleSource$sequence = SurfaceRules$SequenceRuleSource.getDeclaredField("sequence");
-          SurfaceRules$SequenceRuleSource$sequence.setAccessible(true);
 
-          if (SurfaceRules$SequenceRuleSource.isInstance(currentRuleSource)){
-            customSurfaceRules.addAll((List<SurfaceRules.RuleSource>)SurfaceRules$SequenceRuleSource$sequence.get(currentRuleSource));
+          if (currentRuleSource instanceof SurfaceRules.SequenceRuleSource(List<SurfaceRules.RuleSource> sequence)){
+            customSurfaceRules.addAll(sequence);
             return SurfaceRules.sequence(customSurfaceRules.toArray(SurfaceRules.RuleSource[]::new));
           }else{
             customSurfaceRules.add(currentRuleSource);
             return SurfaceRules.sequence(customSurfaceRules.toArray(SurfaceRules.RuleSource[]::new));
           }
-        }catch (Exception e){
-          throw new RuntimeException(e);
-        }
     }
 
     private static SurfaceRules.RuleSource preliminarySurfaceRule(ResourceKey<Biome> biomeKey, BlockState groundBlock, BlockState undergroundBlock, BlockState underwaterBlock) {
