b976778e0434de9da76e1363c48f3bf14105be42
Konkitoman <konkitoman@users.noreply.github.com>
2025-08-13 17:29:29 +0300

    now wood stuff can burn

--- src/main/java/dev/nyxane/mods/scalmyth/datagen/ModDataMapProvider.java
+++ src/main/java/dev/nyxane/mods/scalmyth/datagen/ModDataMapProvider.java
@@ -18,6 +18,17 @@ public class ModDataMapProvider extends DataMapProvider {
     protected void gather(HolderLookup.Provider provider) {
         this.builder(NeoForgeDataMaps.FURNACE_FUELS)
                 .add(ModItems.ASHEN_LOG.getId(), new FurnaceFuel(300), false)
-                .add(ModItems.STRIPPED_ASHEN_LOG.getId(), new FurnaceFuel(300), false);
+                .add(ModItems.STRIPPED_ASHEN_LOG.getId(), new FurnaceFuel(300), false)
+                .add(ModItems.ASHEN_PLANKS.getId(), new FurnaceFuel(300/4), false)
+                .add(ModItems.ASHEN_WOOD.getId(), new FurnaceFuel(300), false)
+                .add(ModItems.STRIPPED_ASHEN_WOOD.getId(), new FurnaceFuel(300), false)
+                .add(ModItems.ASHEN_DOOR.getId(), new FurnaceFuel(300), false)
+                .add(ModItems.ASHEN_SLAB.getId(), new FurnaceFuel(300), false)
+                .add(ModItems.ASHEN_STAIR.getId(), new FurnaceFuel(300), false)
+                .add(ModItems.ASHEN_FENCE.getId(), new FurnaceFuel(300), false)
+                .add(ModItems.ASHEN_FENCE_GATE.getId(), new FurnaceFuel(300), false)
+                .add(ModItems.ASHEN_PRESSURE_PLATE.getId(), new FurnaceFuel(300), false)
+                .add(ModItems.ASHEN_BUTTON.getId(), new FurnaceFuel(100), false)
+                .add(ModItems.ASHEN_TRAPDOOR.getId(), new FurnaceFuel(300), false);
     }
 }
