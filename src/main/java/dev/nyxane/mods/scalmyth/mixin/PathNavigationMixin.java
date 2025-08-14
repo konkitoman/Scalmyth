bd0955e3477537ecb0022da0da65e947cb09a93b
Konkitoman <konkitoman@users.noreply.github.com>
2025-08-14 20:34:22 +0300

    Fix the forge AI bug by reverting to vanila behaviour
    when you are facing: west (Towards negative X) or
    north (Towards negative Z) the path finding will fail to reach you,
    the entity needs to come towards your back for the bug to happen.
    
    forge 47.4.6 is not affected but 48.0.0 is affected.
    
    forge 48.0.0 is minecraft 1.20.2
    forge 47.4.6 is minecraft 1.20.1
    
    forge 47.4.6 .. forge 48.0.0

---
+++ src/main/java/dev/nyxane/mods/scalmyth/mixin/PathNavigationMixin.java
@@ -0,0 +1,62 @@
+package dev.nyxane.mods.scalmyth.mixin;
+
+import net.minecraft.core.Vec3i;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
+import net.minecraft.world.level.pathfinder.Path;
+import net.minecraft.world.level.pathfinder.PathType;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PathNavigation.class)
+public class PathNavigationMixin {
+    @Shadow
+    protected Vec3 getTempMobPos() {
+        throw new RuntimeException("A shadow method was called");
+    }
+
+    @Shadow
+    protected void doStuckDetection(Vec3 vec3) {
+        throw new RuntimeException("A shadow method was called");
+    }
+    @Final
+    @Shadow
+    protected Mob mob;
+
+    @Shadow
+    protected float maxDistanceToWaypoint;
+    @Shadow
+    protected Path path;
+
+    @Shadow
+    public boolean canCutCorner(PathType pathType) {
+        throw new RuntimeException("A shadow method was called");
+    }
+
+    @Shadow
+    private boolean shouldTargetNextNodeInDirection(Vec3 vec) {
+        throw new RuntimeException("A shadow method was called");
+    }
+
+    @Inject(method = "followThePath", at = @At("HEAD"), cancellable = true)
+    private void followThePath(CallbackInfo ci) {
+        Vec3 vec3 = getTempMobPos();
+        maxDistanceToWaypoint = mob.getBbWidth() > 0.75F ? this.mob.getBbWidth() / 2.0F : 0.75F - this.mob.getBbWidth() / 2.0F;
+        Vec3i vec3i = path.getNextNodePos();
+        double d0 = Math.abs(mob.getX() - ((double)vec3i.getX() + 0.5F));
+        double d1 = Math.abs(mob.getY() - (double)vec3i.getY());
+        double d2 = Math.abs(mob.getZ() - ((double)vec3i.getZ() + 0.5F));
+        boolean flag = d0 < this.maxDistanceToWaypoint && d2 < this.maxDistanceToWaypoint && d1 < 1.0;
+        if (flag || this.canCutCorner(this.path.getNextNode().type) && this.shouldTargetNextNodeInDirection(vec3)) {
+            this.path.advance();
+        }
+
+        doStuckDetection(vec3);
+        ci.cancel();
+    }
+}
