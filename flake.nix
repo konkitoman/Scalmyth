c4f9cde0410d01b6661049d90c0a3ebbe6fd4c9c
Konkitoman <konkitoman@users.noreply.github.com>
2025-08-18 23:32:55 +0300

    Add development nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        {
+          config,
+          self',
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          libs = with pkgs; [
+            libGL
+            glfw
+            openal
+            flite
+            jdk
+          ];
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              jetbrains.idea-community
+            ];
+            buildInputs = libs;
+            LD_LIBRARY_PATH = lib.makeLibraryPath libs;
+          };
+        };
+    };
+}
