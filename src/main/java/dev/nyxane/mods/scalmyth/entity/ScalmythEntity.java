ea972d65cd68fb0f0460d1d17afde4a6c89ebb18
Konkitoman <konkitoman@users.noreply.github.com>
2025-09-26 17:48:27 +0300

    Using Brain Memories for saturation and made the SmartBrain to save memories.

--- src/main/java/dev/nyxane/mods/scalmyth/entity/ScalmythEntity.java
+++ src/main/java/dev/nyxane/mods/scalmyth/entity/ScalmythEntity.java
@@ -1,7 +1,15 @@
 package dev.nyxane.mods.scalmyth.entity;
 
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.Dynamic;
+import dev.nyxane.mods.scalmyth.api.ScalmythAPI;
 import dev.nyxane.mods.scalmyth.registry.ModSounds;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.damagesource.DamageSource;
@@ -17,6 +25,7 @@ import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.entity.ai.memory.NearestVisibleLivingEntities;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
@@ -38,6 +47,7 @@ import net.tslat.smartbrainlib.api.core.behaviour.custom.target.TargetOrRetaliat
 import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
 import net.tslat.smartbrainlib.api.core.sensor.vanilla.HurtBySensor;
 import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyLivingEntitySensor;
+import net.tslat.smartbrainlib.object.MemoryTest;
 import net.tslat.smartbrainlib.util.BrainUtils;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
@@ -45,7 +55,12 @@ import software.bernie.geckolib.animation.*;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 import javax.annotation.Nullable;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 public class ScalmythEntity extends Monster implements GeoEntity, SmartBrainOwner<ScalmythEntity> {
     protected static final RawAnimation IDLE_1 = RawAnimation.begin().then("idle_1", Animation.LoopType.LOOP);
@@ -54,7 +69,7 @@ public class ScalmythEntity extends Monster implements GeoEntity, SmartBrainOwne
     protected static final RawAnimation WALK = RawAnimation.begin().then("walk", Animation.LoopType.LOOP);
     protected static final RawAnimation WALKING = RawAnimation.begin().then("walking", Animation.LoopType.LOOP);
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
-    private int saturation = 0;
+    static final MemoryModuleType<Integer> SATURATION = Registry.register(BuiltInRegistries.MEMORY_MODULE_TYPE, ScalmythAPI.rl("saturation"), new MemoryModuleType<>(Optional.of(Codec.INT)));
 
 
     public ScalmythEntity(EntityType<? extends Monster> entityType, Level level) {
@@ -62,6 +77,7 @@ public class ScalmythEntity extends Monster implements GeoEntity, SmartBrainOwne
     }
 
     public static void applyDarknessAround(ServerLevel level, Vec3 pos, @Nullable Entity source, int radius) {
+
         MobEffectInstance mobeffectinstance = new MobEffectInstance(MobEffects.DARKNESS, 260, 0, false, false);
         MobEffectUtil.addEffectToPlayersAround(level, source, pos, (double) radius, mobeffectinstance, 200);
     }
@@ -169,6 +185,42 @@ public class ScalmythEntity extends Monster implements GeoEntity, SmartBrainOwne
     }
 
     @Override
+    protected Brain<?> makeBrain(Dynamic<?> dynamic) {
+        Brain brain = super.makeBrain(dynamic);
+
+        SmartBrainProvider provider = new SmartBrainProvider(this);
+        try {
+            Field field$codec = Brain.class.getDeclaredField("codec");
+            field$codec.setAccessible(true);
+            Method method$createMemoryList = SmartBrainProvider.class.getDeclaredMethod("createMemoryList", List.class, List.class);
+            method$createMemoryList.setAccessible(true);
+            Method method$compileTasks = SmartBrainProvider.class.getDeclaredMethod("compileTasks");
+            method$compileTasks.setAccessible(true);
+
+            List<BrainActivityGroup<?>> tasks = (List<BrainActivityGroup<?>>)method$compileTasks.invoke(provider);
+            ImmutableList<MemoryModuleType<?>> memories = (ImmutableList<MemoryModuleType<?>>)method$createMemoryList.invoke(provider, tasks, getSensors());
+
+            Codec<Brain<ScalmythEntity>> codec = Brain.codec(memories,  List.of());
+            field$codec.set(brain, (Supplier) () -> codec);
+
+            DataResult parsed = codec.parse(dynamic);
+            Objects.requireNonNull(parsed);
+            Optional o = parsed.resultOrPartial();
+            if (o.isPresent()){
+                Brain parsed_brain = (Brain)o.get();
+                for (MemoryModuleType<?> memory_type: memories){
+                    brain.setMemory(memory_type, parsed_brain.getMemory(memory_type));
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return brain;
+    }
+
+    @Override
     public BrainActivityGroup<? extends ScalmythEntity> getCoreTasks() {
         return BrainActivityGroup.coreTasks(
             new LookAtTarget()
@@ -183,7 +235,10 @@ public class ScalmythEntity extends Monster implements GeoEntity, SmartBrainOwne
         boolean result = super.doHurtTarget(entity);
         if (!entity.isAlive()) {
             if (entity instanceof CrowEntity) {
+                int saturation = BrainUtils.getMemory(this, SATURATION);
                 saturation += 1;
+                BrainUtils.setMemory(this, SATURATION, saturation);
+
             }
         }
 
@@ -197,11 +252,25 @@ public class ScalmythEntity extends Monster implements GeoEntity, SmartBrainOwne
                 new TargetOrRetaliate<ScalmythEntity>() {
                     @Override
                     protected boolean checkExtraStartConditions(ServerLevel level, ScalmythEntity entity) {
+                        Integer tmp_saturation = BrainUtils.getMemory(entity, SATURATION);
+                        int saturation = 0;
+                        if (tmp_saturation != null){
+                            saturation = tmp_saturation;
+                        }else{
+                            BrainUtils.setMemory(entity, SATURATION, saturation);
+                        }
+
                         return saturation < 2 && super.checkExtraStartConditions(level, entity);
+                    }
+
+                    @Override
+                    protected List<Pair<MemoryModuleType<?>, MemoryStatus>> getMemoryRequirements() {
+                        MemoryTest memories = MemoryTest.builder(5).usesMemory(SATURATION);
+                        memories.addAll(super.getMemoryRequirements());
+                        return memories;
                     }
                 }
                     .attackablePredicate(entity -> entity instanceof CrowEntity)
-
             ),
             new FollowEntity<ScalmythEntity, LivingEntity>()
                 .following(mob -> {
